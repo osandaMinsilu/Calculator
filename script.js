9ec1b5b233e71c0127827056071ed0d88aa89138
osanda Mincilu <kuruppunanayakkaraosandamincil@gmail.com>
2025-02-23 09:58:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function display(value) {
+  var value = (document.getElementById("display").value += value);
+}
+
+function solve() {
+  try {
+    let x = document.getElementById("display").value;
+    let y = math.evaluate(x);
+    document.getElementById("display").value = y;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Backspace") {
+    clear();
+  }
+});
+
+function clear() {
+  document.getElementById("display").value = "";
+}
